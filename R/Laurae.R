cb83aecbde433e8e0e79b6229548a83466ae42ae
Laurae2 <dsoukhavong.cht@gmail.com>
2016-08-27 23:49:26 +0200

    Testing documentation

---
+++ R/Laurae.R
@@ -0,0 +1,6 @@
+#' Laurae's package for (very) advanced Data Science for R
+#'
+#' This pacakge is meant to be used for doing advanced statistics, visualization, and modeling.
+#'
+"_PACKAGE"
+#> [1] "_PACKAGE"
