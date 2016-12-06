1b3b769cc234073df032515552ea23454d73c69e
Laurae2 <dsoukhavong.cht@gmail.com>
2016-12-06 17:39:19 +0100

    Allow slightly more flexible "datatables" for xgb.importance.

--- R/xgb.importance.interactive.R
+++ R/xgb.importance.interactive.R
@@ -4,20 +4,27 @@
 #' 
 #' @param importance Type: xgboost model, data.table, or data.frame. Your xgboost model to get feature importance from. Or a data.table/data.frame.
 #' @param feature_names Type: vector of characters. The feature names to use in the feature importance. If you provide a data.table/data.frame, do not input this argument. Defaults to \code{NULL}.
-#' @param digits Type: integer. The number of digits to print in the table.
-#' @param pageLength Type: integer. The number of features to print in a single page.
-#' @param lengthMenu Type: vector of integers. The selectable number of features to print in a single page.
+#' @param digits Type: integer. The number of digits to print in the table. Defaults to \code{6}.
+#' @param pageLength Type: integer. The number of features to print in a single page. Defaults to \code{20}.
+#' @param lengthMenu Type: vector of integers. The selectable number of features to print in a single page. Defaults to \code{c(5, 10, 15, 20, 25, 50, 100, 500)}.
+#' @param gainColor Type: character. The color assigned to bars for Gain depending on its value per feature. Defaults to \code{"lightgreen"}.
+#' @param coverColor Type: character. The color assigned to bars for Cover depending on its value per feature. Defaults to \code{"lightblue"}.
+#' @param freqColor Type: character. The color assigned to bars for Frequency depending on its value per feature. Defaults to \code{"lightgrey"}.
 #' 
-#' @return The datatable printed.
+#' @return The "datatable" (not data.table) printed.
 #' 
 #' @examples
 #' \dontrun{
-#' # Todo
+#' ## my_model <- xgb.train(...)
+#' xgb.importnace.interactive(importance = my_model,
+#'                            feature_names = colnames(data),
+#'                            digits = 6,
+#'                            pageLength = 10)
 #' }
 #' 
 #' @export
 
-xgb.importance.interactive <- function(importance, feature_names = NULL, digits = 6, pageLength = 20, lengthMenu = c(5, 10, 15, 20, 25, 50, 100, 500)) {
+xgb.importance.interactive <- function(importance, feature_names = NULL, digits = 6, pageLength = 20, lengthMenu = c(5, 10, 15, 20, 25, 50, 100, 500), gainColor = "lightgreen", coverColor = "lightblue", freqColor = "lightgrey") {
   
   if (!is.data.frame(importance)) {
     importance <- as.data.frame(xgb.importance(feature_names = feature_names, model = importance))
@@ -29,17 +36,17 @@ xgb.importance.interactive <- function(importance, feature_names = NULL, digits 
             options = list(pageLength = pageLength,
                            lengthMenu = lengthMenu)
   ) %>% formatStyle('Gain',
-                    background = styleColorBar(range(importance$Gain, na.rm = TRUE, finite = TRUE), 'lightgreen'),
+                    background = styleColorBar(range(importance$Gain, na.rm = TRUE, finite = TRUE), gainColor),
                     backgroundSize = '100% 90%',
                     backgroundRepeat = 'no-repeat',
                     backgroundPosition = 'center') %>%
     formatStyle('Cover',
-                background = styleColorBar(range(importance$Cover, na.rm = TRUE, finite = TRUE), 'lightblue'),
+                background = styleColorBar(range(importance$Cover, na.rm = TRUE, finite = TRUE), coverColor),
                 backgroundSize = '100% 90%',
                 backgroundRepeat = 'no-repeat',
                 backgroundPosition = 'center') %>%
     formatStyle('Frequency',
-                background = styleColorBar(range(importance$Frequency, na.rm = TRUE, finite = TRUE), 'lightgrey'),
+                background = styleColorBar(range(importance$Frequency, na.rm = TRUE, finite = TRUE), freqColor),
                 backgroundSize = '100% 90%',
                 backgroundRepeat = 'no-repeat',
                 backgroundPosition = 'center') %>%
