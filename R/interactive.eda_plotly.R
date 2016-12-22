687e3c1fa57ed23938c07aa19de0585b374d761a
Laurae2 <dsoukhavong.cht@gmail.com>
2016-12-22 14:58:39 +0100

    Fix factor clustering on Plotly & depth=0 crashes

--- R/interactive.eda_plotly.R
+++ R/interactive.eda_plotly.R
@@ -92,7 +92,7 @@ interactive.eda_plotly <- function(data,
           conditionalPanel(condition = "input.plot_var == 'geom_density' || input.plot_var == 'geom_density_2d' || input.plot_var == 'stat_density_2d' || input.plot_var == 'geom_bar' || input.plot_var == 'geom_point' || input.plot_var == 'geom_boxplot'",
                            sliderInput("continuous_var", "Continuous Grouping Constraint Strictness:", min = 0, max = 0.1, value = 0.001)),
           conditionalPanel(condition = "input.plot_var == 'geom_density' || input.plot_var == 'geom_density_2d' || input.plot_var == 'stat_density_2d' || input.plot_var == 'geom_bar' || input.plot_var == 'geom_point' || input.plot_var == 'geom_boxplot'",
-                           numericInput("categories_var", "Maximum Categories (31):", value = 4, min = 0)),
+                           numericInput("categories_var", "Maximum Categories (31):", value = 4, min = 1)),
           conditionalPanel(condition = "input.plot_var == 'scatter' || input.plot_var == 'box' || input.plot_var == 'polar' || input.plot_var == 'scatter3d'",
                            selectInput("symbol_var", "Symbol Mode:", choices = c("None", "circle" , "circle-open" , "circle-dot" , "circle-open-dot" , "square" , "square-open" , "square-dot" , "square-open-dot" , "diamond" , "diamond-open" , "diamond-dot" , "diamond-open-dot" , "cross" , "cross-open" , "cross-dot" , "cross-open-dot" , "x" , "x-open" , "x-dot" , "x-open-dot" , "triangle-up" , "triangle-up-open" , "triangle-up-dot" , "triangle-up-open-dot" , "triangle-down" , "triangle-down-open" , "triangle-down-dot" , "triangle-down-open-dot" , "triangle-left" , "triangle-left-open" , "triangle-left-dot" , "triangle-left-open-dot" , "triangle-right" , "triangle-right-open" , "triangle-right-dot" , "triangle-right-open-dot" , "triangle-ne" , "triangle-ne-open" , "triangle-ne-dot" , "triangle-ne-open-dot" , "triangle-se" , "triangle-se-open" , "triangle-se-dot" , "triangle-se-open-dot" , "triangle-sw" , "triangle-sw-open" , "triangle-sw-dot" , "triangle-sw-open-dot" , "triangle-nw" , "triangle-nw-open" , "triangle-nw-dot" , "triangle-nw-open-dot" , "pentagon" , "pentagon-open" , "pentagon-dot" , "pentagon-open-dot" , "hexagon" , "hexagon-open" , "hexagon-dot" , "hexagon-open-dot" , "hexagon2" , "hexagon2-open" , "hexagon2-dot" , "hexagon2-open-dot" , "octagon" , "octagon-open" , "octagon-dot" , "octagon-open-dot" , "star" , "star-open" , "star-dot" , "star-open-dot" , "hexagram" , "hexagram-open" , "hexagram-dot" , "hexagram-open-dot" , "star-triangle-up" , "star-triangle-up-open" , "star-triangle-up-dot" , "star-triangle-up-open-dot" , "star-triangle-down" , "star-triangle-down-open" , "star-triangle-down-dot" , "star-triangle-down-open-dot" , "star-square" , "star-square-open" , "star-square-dot" , "star-square-open-dot" , "star-diamond" , "star-diamond-open" , "star-diamond-dot" , "star-diamond-open-dot" , "diamond-tall" , "diamond-tall-open" , "diamond-tall-dot" , "diamond-tall-open-dot" , "diamond-wide" , "diamond-wide-open" , "diamond-wide-dot" , "diamond-wide-open-dot" , "hourglass" , "hourglass-open" , "bowtie" , "bowtie-open" , "circle-cross" , "circle-cross-open" , "circle-x" , "circle-x-open" , "square-cross" , "square-cross-open" , "square-x" , "square-x-open" , "diamond-cross" , "diamond-cross-open" , "diamond-x" , "diamond-x-open" , "cross-thin" , "cross-thin-open" , "x-thin" , "x-thin-open" , "asterisk" , "asterisk-open" , "hash" , "hash-open" , "hash-dot" , "hash-open-dot" , "y-up" , "y-up-open" , "y-down" , "y-down-open" , "y-left" , "y-left-open" , "y-right" , "y-right-open" , "line-ew" , "line-ew-open" , "line-ns" , "line-ns-open" , "line-ne" , "line-ne-open" , "line-nw" , "line-nw-open"), selected = "circle")),
           conditionalPanel(condition = "input.plot_var == 'bar' || input.plot_var == 'box'",
@@ -190,12 +190,19 @@ interactive.eda_plotly <- function(data,
           mini_data <- data.table(input = input, dep = input)
         }
         temp_model <- rpart(dep ~ input, data = mini_data, control = rpart.control(cp = continuous, maxcompete = 1, xval = 10, maxsurrogate = 0, maxdepth = categories))
-        mini_data <- eval(parse(text = "mini_data[, predicted := as.factor(rpart:::pred.rpart(temp_model, rpart:::rpart.matrix(mini_data)))]"))
-        mini_agg <- mini_data[, list(mini = min(input, na.rm = TRUE), maxi = max(input, na.rm = TRUE)), by = predicted]
-        mini_agg <- mini_agg[order(mini), ]
-        mini_agg <- mini_agg[, ordered := as.factor(1:nrow(mini_agg))]
-        levels(mini_agg[["ordered"]]) <- paste("[", mini_agg[["mini"]], ", ", mini_agg[["maxi"]], "]", sep = "")
-        mini_data <- merge(mini_data[, "predicted", with = FALSE], mini_agg[, c("predicted", "ordered")], by = "predicted")
+        if (is.factor(input)) {
+          mini_data <- mini_data[, ordered := as.factor(predict(as.party(temp_model), type = "node"))]
+          mini_agg <- mini_data[!duplicated(mini_data[, c("dep", "ordered"), with = FALSE]), c("dep", "ordered"), with = FALSE]
+          mini_agg <- dcast(mini_agg, ordered ~ dep, value.var = "ordered")
+          levels(mini_data[["ordered"]]) <- apply(mini_agg, 1, function(x) {temp <- x[(2:length(x))[!is.na(x[2:length(x)])]]; return(paste(names(temp), collapse = ", "))})
+        } else {
+          mini_data <- mini_data[, predicted := as.factor(predict(as.party(temp_model), type = "node"))]
+          mini_agg <- mini_data[, list(mini = min(input, na.rm = TRUE), maxi = max(input, na.rm = TRUE)), by = predicted]
+          mini_agg <- mini_agg[order(mini), ]
+          mini_agg <- mini_agg[, ordered := as.factor(1:nrow(mini_agg))]
+          levels(mini_agg[["ordered"]]) <- paste("[", mini_agg[["mini"]], ", ", mini_agg[["maxi"]], "]", sep = "")
+          mini_data <- merge(mini_data[, "predicted", with = FALSE], mini_agg[, c("predicted", "ordered")], by = "predicted")
+        }
         return(mini_data[["ordered"]])
       }
       
